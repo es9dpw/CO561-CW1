38742bc77f6bf2422927ac72a15acb5b050d7cee
es9dpw <es9dpw999@gmail.com>
2024-05-09 16:17:38 +0100

    commit

--- program.fs
+++ program.fs
@@ -1,8 +1,28 @@
-﻿// Learn more about F# at http://fsharp.org
+﻿open System
 
-open System
+type Account = { 
+accountNumber : string
+mutable balance : float
+}
 
-[<EntryPoint>]
-let main argv =
-    printfn "Hello World from F#!"
-    0 // return an integer exit code
+let account1 = { accountNumber = "11111111"; balance = 1000 }
+
+Console.WriteLine(account1)
+
+Console.Write("Do you want to 1.Withdraw or 2.Deposit: ")
+let option : int = int(Console.ReadLine())
+
+
+if option = 1 then
+    Console.Write("Enter the amount you want to withdraw: ")
+    let mutable amount : float = int(Console.ReadLine())
+    if account1.balance >= amount then
+        account1.balance <- account1.balance - amount
+        Console.WriteLine(account1)
+    else
+        Console.Write("There are insufficient funds to withdraw this amount")
+else if option = 2 then
+    Console.Write("Enter the amount you want to withdraw: ")
+    let mutable amount : float = int(Console.ReadLine())
+    account1.balance <- account1.balance + amount
+    Console.WriteLine(account1)
